8e96c91a150a38e9dd00c929180cc94399315075
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-03-25 01:34:10 +0300

    Создание класса Salad и реализация его конструктора

---
+++ l1/l1/ConsoleApplication1/Salad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Salad
+    {
+        private string name;
+        public Salad()
+        {
+            name = "Салат без названия";
+        }
+        public Salad(string name)
+        {
+            this.name = name;
+        }
+    }
+}

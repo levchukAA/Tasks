e357d153093a91f1d1136aa3b829c19448d05d11
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-03-25 10:41:31 +0300

    Создание класса Ingredient, создание в нём свойств Name, Weight, Calories

---
+++ l1/l1/ConsoleApplication1/Ingredient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Ingredient
+    {
+        private double weight;
+        private string name;
+        private double kcal100g;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Calories
+        {
+            get { return kcal100g; }
+            set { kcal100g = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+}

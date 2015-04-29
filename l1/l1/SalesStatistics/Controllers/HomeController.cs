a791e83777f35d3298a9c8faa9b87ca2090c1190
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-30 02:44:55 +0300

    Creating example mvc project Books

---
+++ l1/l1/SalesStatistics/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalesStatistics.Models;
+
+namespace SalesStatistics.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все полученный объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+
+    }
+}
---
+++ l1/l1/SalesStatistics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalesStatistics.MvcApplication" Language="C#" %>

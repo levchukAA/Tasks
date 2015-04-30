a867f14c227a041bd019d2e218d004a2158ed136
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-30 20:09:55 +0300

    Changing Database in SaleProject for transfer information to Database of MVCSaleProject.

--- l1/l1/SaleProject/Program.cs
+++ l1/l1/SaleProject/Program.cs
@@ -1,16 +1,6 @@
 ﻿using System;
 using System.CodeDom;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Data.Entity;
-using System.Data.Odbc;
-using System.Data.OleDb;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SaleProject
 {

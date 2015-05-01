8e3a3a6ed1dd484e045e07e485fe8e80ae287f32
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-05-01 17:25:21 +0300

    Add code to fill the sales table.

--- l1/l1/MvcSaleProject/Controllers/HomeController.cs
+++ l1/l1/MvcSaleProject/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using SaleProject;
 
 
 namespace MvcSaleProject.Controllers
@@ -11,6 +13,23 @@ namespace MvcSaleProject.Controllers
     {
         public ActionResult Index()
         {
+            var dataTable = Methods.GetTableCsv(@"D:\epam\Tasks\l1\l1\SaleProject\bin\Debug\6.csv");
+            using (var dataBase = new BloggingContext())
+            {
+                for (var i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    var varTime = (DateTime)dataTable.Rows[i][0];
+                    var record = new ArchiveRecord
+                    {
+                        Date = varTime,
+                        Client = (string)dataTable.Rows[i][1],
+                        Goods = (string)dataTable.Rows[i][2],
+                        Amount = (int)dataTable.Rows[i][3]
+                    };
+                    dataBase.Archive.Add(record);
+                }
+                dataBase.SaveChanges();
+            }
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
             return View();
         }
